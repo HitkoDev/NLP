ddff6a4db675f351f66f94dfc4c5866317533e5a
Matt-Tanti <m.tanti02@gmail.com>
2022-03-05 18:17:14 +0100

    Layout Setup

---
+++ run.py
@@ -0,0 +1,11 @@
+
+
+def main():
+    import classifiers.dnn.classifier as dnn
+
+    classifier = dnn.Classifier() # Fill With Variables
+
+    classifier.classify()
+
+if __name__ == "__main__":
+    main()
