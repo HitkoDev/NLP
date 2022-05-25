5e47070bc180a818c6d2c41dd938778c25685f28
HitkoDev <development@hitko.si>
2022-05-25 03:10:49 +0200

    feat: rt

--- prodigy/retrain.sh
+++ prodigy/retrain.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker run -ti --rm -v $(pwd)/dataset:/nlp -p 3000:3000 registry.htk.io/ds/prodigy "prodigy ner.batch_train stories /nlp/en_core_web_lg_rt -o /nlp/en_core_web_lg_rt -n 10"
+docker run -ti --rm -v $(pwd)/dataset:/nlp -p 3000:3000 registry.htk.io/ds/prodigy "prodigy ner.batch_train stories /nlp/en_core_web_lg_base -o /nlp/en_core_web_lg_rt -n 10"
