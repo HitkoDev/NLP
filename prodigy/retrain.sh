98ab534cea142b18989bf343c7c65294fbbcfc22
HitkoDev <development@hitko.si>
2022-05-25 02:43:10 +0200

    feat: filter unfit stories

---
+++ prodigy/retrain.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+docker run -ti --rm -v $(pwd)/dataset:/nlp -p 3000:3000 registry.htk.io/ds/prodigy "prodigy ner.batch_train stories /nlp/en_core_web_lg_rt -o /nlp/en_core_web_lg_rt -n 10"
