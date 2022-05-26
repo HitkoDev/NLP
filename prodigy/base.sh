c1db73217ebe057e3afa2ffb2ac32f0563d4428c
HitkoDev <development@hitko.si>
2022-05-26 15:02:38 +0200

    feat: stories

--- prodigy/base.sh
+++ prodigy/base.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-docker run -ti --rm -v $(pwd)/dataset:/nlp -p 3000:3000 registry.htk.io/ds/prodigy "prodigy dataset stories"
-docker run -ti --rm -v $(pwd)/dataset:/nlp -p 3000:3000 registry.htk.io/ds/prodigy "prodigy db-in stories /nlp/tokenized/all.jsonl"
-docker run -ti --rm -v $(pwd)/dataset:/nlp -p 3000:3000 registry.htk.io/ds/prodigy "prodigy ner.batch_train stories en_core_web_lg -o /nlp/en_core_web_lg_base -n 10"
+docker run -ti --rm -v $(pwd)/dataset:/nlp registry.htk.io/ds/prodigy "prodigy dataset stories"
+docker run -ti --rm -v $(pwd)/dataset:/nlp registry.htk.io/ds/prodigy "prodigy db-in stories /nlp/tokenized/all.jsonl"
+docker run -ti --rm -v $(pwd)/dataset:/nlp registry.htk.io/ds/prodigy "prodigy ner.batch_train stories en_core_web_lg -o /nlp/en_core_web_lg_base -n 10"
 mv dataset/prodigy.db dataset/prodigy.base.db
 cp -rT dataset/en_core_web_lg_base dataset/en_core_web_lg_rt
--- prodigy/export.sh
+++ prodigy/export.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker run -ti --rm -v $(pwd)/dataset:/nlp -p 3000:3000 registry.htk.io/ds/prodigy "prodigy db-out stories /nlp/out"
+docker run -ti --rm -v $(pwd)/dataset:/nlp registry.htk.io/ds/prodigy "prodigy db-out stories /nlp/out"
