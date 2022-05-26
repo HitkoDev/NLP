087ec5f8b1755e9aaaa5b536641f1712717d9a87
HitkoDev <development@hitko.si>
2022-05-26 14:58:17 +0200

    docs: readme

--- prodigy/base.sh
+++ prodigy/base.sh
@@ -3,3 +3,4 @@ docker run -ti --rm -v $(pwd)/dataset:/nlp -p 3000:3000 registry.htk.io/ds/prodi
 docker run -ti --rm -v $(pwd)/dataset:/nlp -p 3000:3000 registry.htk.io/ds/prodigy "prodigy db-in stories /nlp/tokenized/all.jsonl"
 docker run -ti --rm -v $(pwd)/dataset:/nlp -p 3000:3000 registry.htk.io/ds/prodigy "prodigy ner.batch_train stories en_core_web_lg -o /nlp/en_core_web_lg_base -n 10"
 mv dataset/prodigy.db dataset/prodigy.base.db
+cp -rT dataset/en_core_web_lg_base dataset/en_core_web_lg_rt
